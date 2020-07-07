e66e2d516cb01f40f4409004133ea33e6d0b3952
Jan Mastalski <jan.mastalski@kiongroup.com>
2020-07-07 14:15:18 +0200

    Add project files

---
+++ console-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace weather_widget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
